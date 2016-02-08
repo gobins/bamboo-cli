065c580ec387737e9071bffc717efe776c0849d0
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-02-08 21:17:04 +1100

    Validated authentication

--- auth.go
+++ auth.go
@@ -1,7 +1,9 @@
-package bambooapi
+package bamboocli
 
 import (
+	"bufio"
 	"net/http"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -12,13 +14,33 @@ type credentials struct {
 	baseurl  string
 }
 
-func basicAuth(cred credentials) {
+func getAuthToken(cred credentials) *http.Response {
 	log.Debug("basicAuth to bamboo server")
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", cred.baseurl, nil)
 	req.SetBasicAuth(cred.username, cred.password)
 	resp, err := client.Do(req)
 	if err != nil {
-
+		log.Error("Authentication Error")
 	}
+	log.Debug(resp)
+	// log.Info(resp.Header)
+	// for k, v := range resp.Header {
+	// 	log.Println("key:", k, "value:", v)
+	// }
+	log.Info(resp.Header.Get("Set-Cookie value"))
+	reader := bufio.NewReader(strings.NewReader(resp.Header.Get("Set-Cookie")))
+
+	logReq, err := http.ReadRequest(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(logReq.Header)
+	// cookies := resp.Cookies()
+	// for _, cookie := range cookies {
+	// 	log.Info(cookie)
+	// }
+
+	return resp
 }
---
+++ auth_test.go
@@ -0,0 +1,17 @@
+package bamboocli
+
+import "testing"
+
+func TestGetAuthToken(t *testing.T) {
+	cred := new(credentials)
+
+	cred.baseurl = "http://localhost:8085"
+	cred.username = "admin"
+	cred.password = "password"
+
+	resp := getAuthToken(*cred)
+
+	if resp == nil {
+		t.Error("Error")
+	}
+}
