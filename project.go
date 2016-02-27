0d395a2e9393acadd9b5b594257753df562bb2f3
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-02-27 20:55:31 +1100

    getAllProjects return slice of project

--- project.go
+++ project.go
@@ -11,14 +11,31 @@ import (
 type project struct {
 	Name string `xml:"name,attr"`
 	Key  string `xml:"key,attr"`
-	Link string `xml:"key,href"`
+	Link Link   `xml:"link"`
 }
 
-type Allprojects struct {
-	projects []project
+//Link attribute which holds the url to the object
+type Link struct {
+	Href string `xml:"href,attr"`
 }
 
-func getAllProjects(cred credentials) {
+//AllProjects parent element for all projects
+type AllProjects struct {
+	Projects Projects `xml:"projects"`
+	Link     Link     `xml:"link"`
+}
+
+//Projects containing multiple project definition
+type Projects struct {
+	Projects []project `xml:"project"`
+}
+
+// type Allprojects struct {
+// 	//XMLName  xml.Name  `xml:projects"`
+// 	projects []project `xml:project"`
+// }
+
+func getAllProjects(cred credentials) []project {
 	log.Debug("Retrieving all projects")
 	token := getAuthToken(cred)
 	client := &http.Client{}
@@ -26,23 +43,17 @@ func getAllProjects(cred credentials) {
 	tokencookie := &http.Cookie{Name: "atl.xsrf.token", Value: token, HttpOnly: false}
 	req.AddCookie(tokencookie)
 	resp, err := client.Do(req)
+	var pr AllProjects
 	if err != nil {
 		log.Error("Error retrieving projects")
 	} else {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
-
 		if err != nil {
 			log.Error("Error reading response body")
 		}
-		log.Info(string(body))
-
-		var pr Allprojects
-
+		log.Debug(string(body))
 		xml.Unmarshal(body, &pr)
-
-		log.Info(pr.projects)
 	}
-
-	//log.Info(resp.Body)
+	return pr.Projects.Projects
 }
