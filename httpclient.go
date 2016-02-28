4d8ee52feed8091edd37c7e796e4da5779212e55
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-02-28 18:29:18 +1100

    Adding httpclient to handle all calls

--- httpclient.go
+++ httpclient.go
@@ -6,17 +6,21 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-type HTTPClient struct {
-}
-
-func (client HTTPClient) httpclient(cred credentials, method string) {
+func httpclient(cred credentials, method string) *http.Response {
 	log.Debug("httpclient function")
 	if cred.token == "" {
 		cred.token = getAuthToken(cred)
 	}
-	client := &http.Client{}
+	httpclient := &http.Client{}
 	req, _ := http.NewRequest(method, cred.baseurl+cred.apiuri, nil)
+
+	//Adding auth cookie to call bamboo REST API
 	tokencookie := &http.Cookie{Name: "atl.xsrf.token", Value: cred.token, HttpOnly: false}
 	req.AddCookie(tokencookie)
-	resp, err := client.Do(req)
+	resp, err := httpclient.Do(req)
+	if err != nil {
+		log.Error("Error calling url:", cred.baseurl+cred.apiuri)
+	}
+	log.Debug(resp)
+	return resp
 }
